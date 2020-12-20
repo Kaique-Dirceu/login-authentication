771f642ec1016d7c283a1a605d685fc7999b2eb0
Kaique Dirceu <dirceukaique@gmail.com>
2020-12-19 22:02:37 -0300

    add login layout

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,6 +1,32 @@
+import Imagem from 'next/image';
+import { useState } from 'react';
+import { motion, Variants } from 'framer-motion'
+
 const login: React.FC = () => {
   return (
-    <h1>Login</h1>
+    <div className="flex flex-1 justify-center items-center h-screen w-screen bg-gray-900">
+      <form className="flex justify-center items-center flex-col">
+        <motion.img 
+          className="h-80 w-80 -m-28"
+          src="/img/login.svg" 
+          layoutId="logoLogin"
+        />
+        <input
+          className="text-white m-2 bg-gray-500 text-gray-50 px-6 py-3 text-lg font-semibold rounded-xl hover:bg-gray-700 "
+          type="email"
+          placeholder="email"
+        />
+        <input
+          className="text-white m-2 bg-gray-500 text-gray-50 px-6 py-3 text-lg font-semibold rounded-xl hover:bg-gray-700 "
+          type="password"
+          placeholder="senha"
+        />
+
+        <button 
+        className="m-4 bg-indigo-900 text-gray-300 font-bold text-2xl h-12 w-40 rounded-2xl hover:bg-indigo-800"
+        type="submit">Logar</button>
+      </form>
+    </div>
   )
 }
 
